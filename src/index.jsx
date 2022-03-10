1d90f16f4590146a64b056b70f558fb5bf72ff92
Bruno <brunocostaprado@gmail.com>
2022-03-10 02:18:41 -0300

    0

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-const { QueryClient, QueryClientProvider, useQuery } = require("react-query");
+const { QueryClient, QueryClientProvider, useQuery, useMutation } = require("react-query");
 
 const queryClient = new QueryClient();
 
@@ -18,11 +18,25 @@ return res.json();
 }
 
 function Cars(){
-    const{data, status}=useQuery('cars',fetchCars);
+    const{data, status, isFetching}=useQuery('cars',fetchCars);
     if(status==='loading'){
         return <p>Loading...</p>;
     }
     if(status==='error'){
         return <p>Error!</p>;
     }
+const mutation=useMutation(postTodo,{
+    onSuccess:()=>{
+        queryClient.invalidateQueries('cars')
+    },
+})
+
+    return(
+    <ul>
+        {data.map((car) =>(
+            <li key={car.id}>{car.make}</li>)
+        )}
+        {isFetching &&<p>Atualizando dados...</p>}
+    </ul>
+    );
 }
