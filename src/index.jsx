eda237e888ebcd9cf115ce1d8de8fb1ea0221fa1
Bruno <brunocostaprado@gmail.com>
2022-03-10 03:00:00 -0300

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 const { QueryClient, QueryClientProvider, useQuery, useMutation } = require("react-query");
-
+import {postTodo } from '../my-api'
 const queryClient = new QueryClient();
+ import { ReactQueryDevtools } from 'react-query/devtools'
 
 function App(){
 
     return(
         <QueryClientProvider client={queryClient}>
             <Cars />
-            <ReactQueryDevtools>
+            <ReactQueryDevtools initialIsOpen={true}/> 
             
         </QueryClientProvider>
     )
@@ -17,26 +18,37 @@ async function fetchCars(){
 return res.json();
 }
 
-function Cars(){
-    const{data, status, isFetching}=useQuery('cars',fetchCars);
-    if(status==='loading'){
-        return <p>Loading...</p>;
-    }
-    if(status==='error'){
-        return <p>Error!</p>;
-    }
-const mutation=useMutation(postTodo,{
-    onSuccess:()=>{
-        queryClient.invalidateQueries('cars')
-    },
-})
+    function Cars(){
+        const{data, status, isFetching}=useQuery('cars',fetchCars);
+        if(status==='loading'){
+            return <p>Loading...</p>;
+        }
+        if(status==='error'){
+            return <p>Error!</p>;
+        }
+    const mutation=useMutation(postTodo,{
+        onSuccess:()=>{
+            queryClient.invalidateQueries('cars')
+        },
+    })
 
-    return(
-    <ul>
-        {data.map((car) =>(
-            <li key={car.id}>{car.make}</li>)
-        )}
-        {isFetching &&<p>Atualizando dados...</p>}
-    </ul>
-    );
-}
+        return(
+        <div>
+            <ul>
+                {data.map((car) =>(
+                    <li key={car.id}>{car.make}</li>)
+                )}
+                {isFetching &&<p>Atualizando dados...</p>}
+            </ul>
+            <button onClick={() => {
+            mutation.mutate({
+                id: Date.now(),
+                title: 'Do Laundry',
+            })
+            }}
+        >
+            Add
+        </button>
+        </div>);
+    }
+render(<App />, document.getElementById('root'))
